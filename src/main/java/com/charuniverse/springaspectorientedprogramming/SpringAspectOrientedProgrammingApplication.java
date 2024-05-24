aa9699866ec1ec7a5b080e5700711865b4550014
ahps <akbarputrasrg@gmail.com>
2024-05-24 08:28:22 +0700

    initial commit

---
+++ src/main/java/com/charuniverse/springaspectorientedprogramming/SpringAspectOrientedProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.charuniverse.springaspectorientedprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAspectOrientedProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAspectOrientedProgrammingApplication.class, args);
+	}
+
+}
