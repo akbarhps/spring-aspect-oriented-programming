0e90bdc8ec1c3b96a1b7fa547dd7f690fe74ccb6
ahps <akbarputrasrg@gmail.com>
2024-05-24 08:39:44 +0700

    course d: pointcut

--- src/main/java/com/charuniverse/springaspectorientedprogramming/aspect/LogAspect.java
+++ src/main/java/com/charuniverse/springaspectorientedprogramming/aspect/LogAspect.java
@@ -1,10 +1,15 @@
 package com.charuniverse.springaspectorientedprogramming.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogAspect {
+
+    @Pointcut("")
+    public void helloServiceMethod() {
+    }
 
 }
