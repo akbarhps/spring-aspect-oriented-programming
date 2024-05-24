72b026fb98de44af3f7ea3addfbdefcf991137e1
ahps <akbarputrasrg@gmail.com>
2024-05-24 08:47:27 +0700

    course f: advice

--- src/main/java/com/charuniverse/springaspectorientedprogramming/aspect/LogAspect.java
+++ src/main/java/com/charuniverse/springaspectorientedprogramming/aspect/LogAspect.java
@@ -1,9 +1,12 @@
 package com.charuniverse.springaspectorientedprogramming.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Aspect
 @Component
 public class LogAspect {
@@ -12,4 +15,13 @@ public class LogAspect {
     public void helloServiceMethod() {
     }
 
+    @Before("helloServiceMethod()")
+    public void beforeHelloServiceMethod() {
+        log.info("Before HelloService method");
+    }
+
+    @Before("helloServiceMethod()")
+    public void beforeHelloServiceMethod2() {
+        log.info("Before HelloService method Again");
+    }
 }
