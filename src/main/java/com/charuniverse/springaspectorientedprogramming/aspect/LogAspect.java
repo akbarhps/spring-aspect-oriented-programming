2ca9cce0959211e9b06bc5b410892fcae547b0e1
ahps <akbarputrasrg@gmail.com>
2024-05-24 08:42:52 +0700

    course e: pointcut expression

--- src/main/java/com/charuniverse/springaspectorientedprogramming/aspect/LogAspect.java
+++ src/main/java/com/charuniverse/springaspectorientedprogramming/aspect/LogAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAspect {
 
-    @Pointcut("")
+    @Pointcut("target(com.charuniverse.springaspectorientedprogramming.service.HelloService)")
     public void helloServiceMethod() {
     }
 
